0e29e19c22749c76b51b03893cab77047f3c2cf6
Lerio <764109520@qq.com>
2018-11-08 18:01:38 +0800

    Add files via upload

---
+++ config/global.config.php
@@ -0,0 +1,26 @@
+﻿<?php
+/**
+ * [$Config 全局配置]
+ * @var array
+ */
+$Config = array(
+
+	/*数据库设置*/
+	'database' => array('db_host' => 'localhost',//mysql主机地址
+						'db_user' => 'root',//用户名
+						'db_psw' => 'root',//密码
+						'db_name' => 'test',//数据库
+						'db_provider' => 'mysql'),//提供者
+
+	/*基本设置*/
+	'setting' => array('encryptkey' => 'myprivatekey',
+					   'check' => array('timeout' => 3000/*身份校验超时时间s*/)),
+
+	/*缓存服务器设置*/
+	'memcached' => array('host' => '127.0.0.1',
+					 'port' => 11211,
+					 'prefix' => 'myprefix',
+					 'expired' => 5*60/*统一失效时间ms*/)
+
+	);
+?>
